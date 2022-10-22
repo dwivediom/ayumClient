0cccf791d73dc4819d152877264542b093f3aaff
dwivediom00 <dwivediom00.com>
2022-10-22 19:39:09 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const runtimeCaching = require("next-pwa/cache");
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+});
+
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+    swcMinify: true,
+});
+module.exports = nextConfig;
